840e1974cb0012c5302e09671af4dca6ce5012ae
Kadir Akyol <102226254+kadirakyol@users.noreply.github.com>
2022-06-07 12:22:31 +0300

    test

--- Assets/BallHandler.cs
+++ Assets/BallHandler.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Camera mainCamera;
+    private bool isDragging;
+
+    [SerializeField] private Rigidbody2D currentBallRigidbody;
+    [SerializeField] private SpringJoint2D currentBallSpringJoint;
+    [SerializeField] private float detachBallTime = 0.1f;
+    
     void Start()
     {
-        // TEST GİT
+        mainCamera = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (currentBallRigidbody == null)
+        {
+            return;
+        }
+        
+        if (!Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            if (isDragging)
+            {
+                LaunchBall();
+            }
+            isDragging = false;
+
+            return; 
+        }
+        isDragging = true;
+        currentBallRigidbody.isKinematic = true;
+
+        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+
+        currentBallRigidbody.position = worldPosition;
+
+    }
+
+    private void LaunchBall()
+    {
+        currentBallRigidbody.isKinematic = false;
+        currentBallRigidbody = null;
+        Invoke("DetachBall", detachBallTime);
+    } 
+
+    private void DetachBall()
+    {
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
         
     }
+
+
+
 }
